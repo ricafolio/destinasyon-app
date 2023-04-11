6796a4bcb84c1f63e04b1be7f530cf36c607b094
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-04-12 07:00:23 +0800

    chore: update input component

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,16 +1,25 @@
+"use client";
+
+import { useState } from "react"
+
 export default function Input() {
+  const [prompt, setPrompt] = useState("")
+
   return (
     <div className="flex flex-row w-full relative">
-      <textarea 
-        placeholder="somewhere cool, not so populated with people" 
+      <textarea
+        name="prompt"
+        placeholder="somewhere cool, not so populated with people"
+        rows={4}
         className="
-          w-full text-2xl pl-4 pr-48 py-4 rounded-lg transition-all duration-300
+          w-full text-2xl pl-4 pr-48 py-4 rounded-lg transition-colors duration-300
           border-2 border-gray-200 hover:bg-gray-50 focus:bg-white
           focus:outline-6 focus:outline-black focus:outline-offset-4
         "
-        rows={4}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
       ></textarea>
-      <div className="absolute right-4 top-4 flex flex-col">
+      <div className="absolute right-4 top-4 flex flex-col w-[160px]">
         <button className="
           bg-black text-white text-xl rounded-lg px-8 py-2 
           transition-colors duration-150
@@ -19,14 +28,14 @@ export default function Input() {
         ">
           Submit
         </button>
-        <button className="
+        {prompt === "" && <button className="
           bg-white text-black text-xl rounded-lg px-8 py-2 mt-1
           transition-colors duration-300
           border-2 border-gray-200 hover:border-black
           focus:ring-2 focus:ring-zinc-400/50 focus:outline-offset-4
         ">
           Random
-        </button>
+        </button>}
       </div>
     </div>
   )
