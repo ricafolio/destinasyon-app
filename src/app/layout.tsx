2778c859261b62109804b43ff8c9fd05ef319b0a
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-04-11 17:15:52 +0800

    chore: load fonts in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,21 @@
+import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
+
 import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+const switzer = localFont({
+  src: [
+    {
+      path: './fonts/Switzer/Switzer-Regular.woff2',
+      weight: '400',
+    },
+    {
+      path: './fonts/Switzer/Switzer-Medium.woff2',
+      weight: '500',
+    }
+  ]
+});
 
 export const metadata = {
   title: 'Destinasyon | Travel Recommendations in the Philippines',
@@ -11,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${inter.className}`}>
       <body>{children}</body>
     </html>
   )
