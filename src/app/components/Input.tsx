74fad605113dd167a8fdb2c075bbf081f765e9b0
Ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-04-12 10:03:34 +0800

    chore: update UI ✨

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -6,35 +6,44 @@ export default function Input() {
   const [prompt, setPrompt] = useState("")
 
   return (
-    <div className="flex flex-row w-full relative">
+    <div className="flex flex-col sm:flex-row w-full relative">
       <textarea
         name="prompt"
-        placeholder="somewhere cool, not so populated with people"
+        placeholder="Somewhere with best island hopping experience"
         rows={4}
         className="
-          w-full text-2xl pl-4 pr-48 py-4 rounded-lg transition-colors duration-300
-          border-2 border-gray-200 hover:bg-gray-50 focus:bg-white
-          focus:outline-6 focus:outline-black focus:outline-offset-4
+          w-full text-2xl pl-4 pr-4 sm:pr-48 py-3 rounded-lg
+          transition-colors duration-150
+          border-2 border-white text-black
+          hover:bg-gray-50 focus:bg-white  selection:bg-black/10
+          focus:border-6 focus:border-amber-500 focus:outline-none
         "
         value={prompt}
         onChange={(e) => setPrompt(e.target.value)}
       ></textarea>
-      <div className="absolute right-4 top-4 flex flex-col w-[160px]">
+
+      <div className="sm:absolute sm:right-4 sm:top-4 flex flex-col w-full sm:w-[160px]">
         <button className="
-          bg-black text-white text-xl rounded-lg px-8 py-2 
+          rounded-lg px-8 py-2 mt-1 sm:mt-0 text-xl
           transition-colors duration-150
-          focus:ring-2 focus:ring-zinc-400/50 focus:outline-offset-4
-          hover:bg-zinc-800 active:bg-zinc-700 focus:bg-zinc-800 
+          focus:outline-6 focus:outline-amber-500
+          bg-amber-400 text-black
+          hover:bg-amber-500 active:bg-amber-400 focus:bg-amber-500
         ">
           Submit
         </button>
         {prompt === "" && <button className="
-          bg-white text-black text-xl rounded-lg px-8 py-2 mt-1
-          transition-colors duration-300
-          border-2 border-gray-200 hover:border-black
-          focus:ring-2 focus:ring-zinc-400/50 focus:outline-offset-4
-        ">
-          Random
+            flex items-center justify-center text-center
+            bg-white text-black rounded-lg px-8 py-2 mt-1
+            transition-colors duration-150
+            border-2 border-gray-200 hover:border-gray-400
+          active:bg-slate-200 focus:bg-slate-50
+          "
+        >
+          <span className="rotate-45">
+            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24"><g fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M0 0h24v24H0z"/><path fill="#000" d="M18.333 2c1.96 0 3.56 1.537 3.662 3.472l.005.195v12.666c0 1.96-1.537 3.56-3.472 3.662l-.195.005H5.667a3.667 3.667 0 0 1-3.662-3.472L2 18.333V5.667c0-1.96 1.537-3.56 3.472-3.662L5.667 2h12.666zM15.5 14a1.5 1.5 0 1 0 0 3a1.5 1.5 0 0 0 0-3zm-7 0a1.5 1.5 0 1 0 0 3a1.5 1.5 0 0 0 0-3zm0-7a1.5 1.5 0 1 0 0 3a1.5 1.5 0 0 0 0-3zm7 0a1.5 1.5 0 1 0 0 3a1.5 1.5 0 0 0 0-3z"/></g></svg>
+          </span>
+          <span className="text-xl ml-2">Random</span>
         </button>}
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import Input from "./components/Input"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center text-center p-24 pt-36 bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-slate-400 via-slate-200 to-slate-50">
-      <h1 className="font-bold text-5xl mb-4">Tell us about your next trip</h1>
+    <main className="flex min-h-screen flex-col items-center text-center p-4 sm:p-24 pt-12 sm:pt-36 m-3 rounded-xl bg-gradient-to-b from-zinc-900 to-zinc-800 text-white">
+      <h1 className="font-bold text-5xl">Tell us about your next trip 🏝️</h1>
       <div className="w-full py-8">
         <Input />
       </div>
-      <p className="block w-full p-4 text-lg bg-white/50 rounded border-2 border-dashed border-gray-300">
-        Tip: The more specific you are about your preferences and needs, the more tailored our recommendations will be to your interests and travel style. ✨
+      <p className="block w-full p-4 text-lg text-gray-300 bg-transparent rounded border-2 border-dashed border-gray-600">
+        <b>Tip:</b> The more specific you are about your preferences, the more tailored our recommendations will be to your interests and travel style. ✨
       </p>
     </main>
   )
