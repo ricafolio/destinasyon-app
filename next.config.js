c3590bd4889bfa949e74f9642347eca5fe454794
Ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-04-13 18:58:12 +0800

    chore: whitelist image hosts

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos'
+      },
+      {
+        protocol: 'https',
+        hostname: 'maps.googleapis.com'
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
