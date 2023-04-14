1643db5d5cc6055c583b44b315ab78e64edb7962
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-04-14 16:16:11 +0800

    chore: update saved destinations page ui

--- src/app/components/DestinationSaved.tsx
+++ src/app/components/DestinationSaved.tsx
@@ -3,19 +3,19 @@ import Image from "next/image"
 
 export default function DestinationSaved({ id, name, description, image, at, onDeleteBtnClick }: DestinationSavedProps) {
   return (
-    <div className="w-full md:w-3/12 pr-2 flex flex-col" key={`saved-spot-${id}`}>
-      <div>
-        <Image src={image} alt={name} width="60" height="60" className="w-full rounded-md transition duration-300 ease-in-out hover:brightness-90 mb-2" />
-        <h2 className="text-xl font-semibold">{name}</h2>
-        <p className="text-gray-300 mt-1 mb-2 md:mb-0 sm:pr-4">{description}</p>
+    <div className="w-3/6 md:w-3/12 flex flex-col items-start rounded bg-white text-black relative group" key={`saved-spot-${id}`}>
+      <div className="text-left">
+        <Image src={image} alt={name} width="60" height="60" className="w-full rounded-md transition duration-300 ease-in-out hover:brightness-90 rounded-b-none" />
+        <div className="text-left pt-2 pb-4 px-3">
+          <small className="font-bold text-orange-600">{at}</small>
+          <h2 className="text-xl font-semibold">{name}</h2>
+          <p className="text-gray-700 mt-1 mb-2 md:mb-0 sm:pr-2">{description}</p>
+        </div>
       </div>
 
-      <div className="my-2">
-        <button className="bg-black hover:bg-zinc-800 text-white px-5 py-3 rounded transition-colors inline-flex items-center justify-center" onClick={() => onDeleteBtnClick(id, name)}>
-          <svg xmlns="http://www.w3.org/2000/svg" className="mr-2" width="1em" height="1em" viewBox="0 0 15 15">
-            <path fill="currentColor" fillRule="evenodd" d="M8 2.75a.5.5 0 0 0-1 0V7H2.75a.5.5 0 0 0 0 1H7v4.25a.5.5 0 0 0 1 0V8h4.25a.5.5 0 0 0 0-1H8V2.75Z" clipRule="evenodd"></path>
-          </svg>
-          Remove
+      <div className="group-hover:block hidden absolute top-2 right-2">
+        <button className="bg-red-600 hover:bg-red-500 text-white px-4 py-2 rounded transition-colors inline-flex items-center justify-center" onClick={() => onDeleteBtnClick(id, name)}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 15 15"><path fill="currentColor" fillRule="evenodd" d="M5.5 1a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4ZM3 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 0 1H11v8a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V4h-.5a.5.5 0 0 1-.5-.5ZM5 4h5v8H5V4Z" clipRule="evenodd"></path></svg>
         </button>
       </div>
     </div>
--- src/app/saved-destinations/page.tsx
+++ src/app/saved-destinations/page.tsx
@@ -22,7 +22,7 @@ export default function MySavedDestinations() {
   }
 
   return (
-    <main className="flex flex-col items-center text-center text-white p-4 sm:p-12 md:p-24 pt-12 sm:pt-24">
+    <main className="flex flex-col items-center text-center text-white p-4 md:p-12 lg:p-24 pt-12 sm:pt-24">
       <Toaster
         position="top-center"
         reverseOrder={false}
@@ -32,7 +32,7 @@ export default function MySavedDestinations() {
       />
 
       <h1 className={`font-bold text-2xl sm:text-3xl mb-8 text-left ${erode.className}`}>Your saved destinations</h1>
-      <div className="flex flex-row flex-wrap">
+      <div className="flex flex-row flex-wrap w-full">
         {places?.map((place, i) => {
           return (
             <DestinationSaved
