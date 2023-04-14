0a64588e67d1bda5cd955bb0b2195b4da5a8f309
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-04-14 11:40:56 +0800

    chore: start other pages

---
+++ src/app/help/page.tsx
@@ -0,0 +1,7 @@
+export default function Help() {
+  return (
+    <main className="flex flex-col items-center text-center text-white p-4 sm:p-12 md:p-24 pt-12 sm:pt-24">
+      <h1 className="font-bold text-2xl sm:text-2xl">Help</h1>
+    </main>
+  )
+}
