b4f4aeea7b0ac9054321072621b8c48254370db5
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-04-14 11:13:25 +0800

    chore: improve AI

--- src/app/api/prompt/route.ts
+++ src/app/api/prompt/route.ts
@@ -8,7 +8,7 @@ if (!process.env.GPT_API_KEY) {
 export async function POST(req: Request) {
   try {
     let { action, prompt }: FormBodyContent = await req.json()
-    let s_temp: number = 0.70
+    let s_temp: number = 0.80
     let s_role: ChatGPTAgent = "system"
 
     if (action !== "submit" && action !== "random") {
@@ -16,21 +16,25 @@ export async function POST(req: Request) {
     }
 
     if (action === "random") {
-      s_temp = 0.60
-      prompt = `You are thinking about your next dream trip. 
+      s_temp = 0.5
+      prompt = `You are thinking about your next dream trip.
 
-      Pick one random unusual experiences like food experiences or outdoor activities. Use that as your reason to travel.
+      Pick one random unusual outdoor activity. Pick random location between Luzon, Visayas or Mindanao.
 
-      Write in first person POV. Maximum of 2 sentences. Use modal verbs of desire. Do not specify specific name of places. `
+      Write in first person POV. Maximum of 2 sentences. Use modal verbs of desire. Use simple language.`
 
     } else if (action === "submit") {
       s_role = "user"
-      prompt = `"${prompt}"
+      prompt = `Prompt: "${prompt}"
 
-First, validate the prompt above. It's fine if the prompt is not clear, but if it's doesn't make any sense, stop now, ignore the next instructions and just reply the following code:
+1. Validate the prompt above. It's fine if the prompt is vague, but if it's doesn't make any sense, stop now, ignore the next steps and just reply the following code:
 { success: false, data: null }
 
-Then, find me random travel destinations in the Philippines with that prompt and provide coordinates. Expand by giving another list of best destinations inside of each places and also define why. Also provide coordinates.
+2. Find me random travel destinations in the Philippines with that prompt. Give me three destinations. Provide coordinates. 
+
+2.1 If there's real city, place, location or region on my prompt, just search inside that place.
+
+3. Expand by giving another list of best spots inside each destinations. Give me three spots. Convince me why it's perfect based on my prompt. Also provide coordinates. 
 
 Answer with array of objects format:
 {
@@ -40,7 +44,7 @@ Answer with array of objects format:
       name: "",
       description: "",
       coordinates: "",
-      places: [
+      spots: [
         {
           name: "",
           description: "",
