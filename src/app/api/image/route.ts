b16da193f759ff1d5e846928fd07db78839b2b37
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-04-15 14:16:53 +0800

    refactor: image api

--- src/app/api/image/route.ts
+++ src/app/api/image/route.ts
@@ -1,50 +1,72 @@
 import { NextResponse } from "next/server"
 
-if (!process.env.GOOGLE_PLACES_API_KEY) {
+const GOOGLE_PLACES_API_KEY = process.env.GOOGLE_PLACES_API_KEY
+
+if (!GOOGLE_PLACES_API_KEY) {
   throw new Error("Missing Maps API key!")
+}
+
+async function fetchPlaceInfo(placeName: string) {
+  const searchPlaceUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?key=${GOOGLE_PLACES_API_KEY}&query=${placeName}`
+  const searchPlaceRes = await fetch(searchPlaceUrl, { method: "GET" })
+  const searchResults = await searchPlaceRes.json()
+  
+  if (searchResults.status !== "OK") {
+    throw new Error("API error")
+  }
+
+  return searchResults.results
+}
+
+async function fetchPlacePhotoUrl(photoId: string) {
+  const photoUrl = `https://maps.googleapis.com/maps/api/place/photo?key=${GOOGLE_PLACES_API_KEY}&photo_reference=${photoId}&maxwidth=500&maxheight=500`
+  // google redirects the image to new url, without api key in the url
+  const photoUrlRes = await fetch(photoUrl, { method: "GET", redirect: "manual" })
+  // the new url is available at response headers
+  const photoUrlRedirect = photoUrlRes.headers.get("location")
+  return photoUrlRedirect
 }
 
 export async function GET(request: Request) {
   try {
+    // expected url: /api/image?query=place+name
     const { searchParams } = new URL(request.url)
-    const place_name = searchParams.get("query")
+    const placeName: string | null = searchParams.get("query")
 
-    // Fetch place info and get image ref id
-    const search_place_url = `https://maps.googleapis.com/maps/api/place/textsearch/json?key=${process.env.GOOGLE_PLACES_API_KEY}&query=${place_name}`
-    const search_place_res = await fetch(search_place_url, { 
-      method: "GET"
-    })
-    const search_results = await search_place_res.json()
-    
-    if (search_results.status !== "OK") {
-      throw new Error("API error")
+    if (!placeName) {
+      throw new Error("Query params not found!")
     }
 
-    // fetch direct image url from google api
-    let photo_id = null
-    if (search_results.results[0]["photos"]) {
-      photo_id = search_results.results[0].photos[0].photo_reference
+    const placeInfo = await fetchPlaceInfo(placeName || "")
+
+    if (placeInfo[0].photos) {
+      // get place photo reference id
+      const photoId = placeInfo[0].photos[0].photo_reference
+      // fetch photo url using reference id we got
+      let photoUrlRedirect: string | null = await fetchPlacePhotoUrl(photoId)
+
+      return NextResponse.json({
+        status: "ok",
+        message: "Place image found",
+        url: photoUrlRedirect,
+      })
+    } else {
+       throw new Error("Place image not found") 
     }
-    const photo_url = `https://maps.googleapis.com/maps/api/place/photo?key=${process.env.GOOGLE_PLACES_API_KEY}&photo_reference=${photo_id}&maxwidth=500&maxheight=500`
 
-    const photo_url_redirect = await fetch(photo_url, { 
-      method: "GET",
-      redirect: "manual" // important to get location value in header!
-    }).then((res) => {
-      return res.headers.get("location")
-    })
-
-    return NextResponse.json({
-      status: "ok",
-      url: photo_url_redirect
-    })
   } catch (e) {
+    let message: string = "Unknown error occurred."
+
+    if (e instanceof Error) {
+      message = e.message
+    } else {
+      message = String(e)
+    }
+
     return NextResponse.json({
       status: "error",
-      message: "Unknown error occurred",
-      result: null
-    }, { 
-      status: 400 
-    })
+      message: message,
+      url: null,
+    }, { status: 400 })
   }
 }
