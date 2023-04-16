81c5bc7ab1c3cdcfb9b2b47839a0751a67335080
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-04-16 15:05:43 +0800

    format: input.tsx

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -21,29 +21,18 @@ export default function Input({ prompt, isFetching, onRandomBtnClick, onSubmitBt
 
       <div className="flex flex-col sm:flex-row sm:justify-between w-full mt-2">
         <div className="flex flex-row items-center">
-          <button
-            className="w-1/2 flex items-center justify-center text-center rounded-lg px-8 py-2 bg-white text-black h-full transition-colors duration-150 hover:bg-slate-100 active:bg-slate-200 focus:bg-slate-50 disabled:cursor-wait"
-            onClick={onRandomBtnClick}
-            disabled={isFetching}
-          >
+          <button className="w-1/2 flex items-center justify-center text-center rounded-lg px-8 py-2 bg-white text-black h-full transition-colors duration-150 hover:bg-slate-100 active:bg-slate-200 focus:bg-slate-50 disabled:cursor-wait" onClick={onRandomBtnClick} disabled={isFetching}>
             <span className="rotate-45">
               <RandomIcon />
             </span>
             <span className="text-lg sm:text-xl ml-2">Random</span>
           </button>
-          <button
-            className="w-1/2 bg-white text-black text-lg sm:text-xl rounded-lg px-8 py-2 transition-colors duration-150 hover:bg-slate-100 active:bg-slate-200 focus:bg-slate-50 h-full ml-2"
-            onClick={() => onPromptValueChange({ newValue: "", isClear: true })}
-          >
+          <button className="w-1/2 bg-white text-black text-lg sm:text-xl rounded-lg px-8 py-2 transition-colors duration-150 hover:bg-slate-100 active:bg-slate-200 focus:bg-slate-50 h-full ml-2" onClick={() => onPromptValueChange({ newValue: "", isClear: true })}>
             Clear
           </button>
         </div>
 
-        <button
-          className="rounded-lg px-8 py-2 mt-2 sm:ml-2 sm:mt-0 text-lg sm:text-xl h-full transition-colors duration-150 focus:outline-6  disabled:cursor-wait focus:outline-amber-500 bg-amber-400 text-black hover:bg-amber-500 active:bg-amber-400 focus:bg-amber-500"
-          onClick={onSubmitBtnClick}
-          disabled={isFetching}
-        >
+        <button className="rounded-lg px-8 py-2 mt-2 sm:ml-2 sm:mt-0 text-lg sm:text-xl h-full transition-colors duration-150 focus:outline-6  disabled:cursor-wait focus:outline-amber-500 bg-amber-400 text-black hover:bg-amber-500 active:bg-amber-400 focus:bg-amber-500" onClick={onSubmitBtnClick} disabled={isFetching}>
           Get destinations
         </button>
       </div>
