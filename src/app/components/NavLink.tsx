01dc32bdd0b29ce92d49f7c8185fedb6c759a5fe
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-04-16 15:09:10 +0800

    format: NavLink.tsx

--- src/app/components/NavLink.tsx
+++ src/app/components/NavLink.tsx
@@ -7,7 +7,7 @@ export default function NavLink({ id, name, link }: NavLinkProps) {
     <Link href={link} className="w-full sm:w-auto py-2 px-2 sm:px-5 inline-flex justify-center text-center items-center transition-colors duration-200 bg-zinc-800 hover:bg-zinc-800/80 rounded text-zinc-300">
       {id === "help" && (
         <span className="mr-2">
-          <HelpIcon/>
+          <HelpIcon />
         </span>
       )}
       <span className="text-sm sm:text-base">{name}</span>
