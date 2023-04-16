7f6720daf4b5c89e38def9f32585cc669ab76e61
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-04-16 15:10:27 +0800

    format: layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,14 @@
 import "./globals.css"
-import Link from "next/link";
+import Link from "next/link"
 import NavLink from "./components/NavLink"
 import { switzer, erode } from "./fonts"
 
 export const metadata = {
   title: "Destinasyon | Discover Travel Destinations in the Philippines",
-  description: "Destinasyon is a travel destination finder app that uses AI technology to suggest the best travel destinations in the Philippines.",
+  description: "Destinasyon is a travel destination finder app that uses AI technology to suggest the best travel destinations in the Philippines."
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={`${switzer.className}`}>
       <body className="relative">
@@ -23,7 +19,9 @@ export default function RootLayout({
             </div>
 
             <div className="w-full sm:flex-1 md:w-1/3 order-1 md:order-2 mb-2 sm:mb-0 flex items-center justify-center sm:justify-start md:justify-center pl-0 sm:pl-2 md:pl-0 h-auto">
-              <Link href="/" className={`text-2xl transition-all duration-300 font-bold select-none cursor-pointer ${erode.className} inline-block text-transparent  bg-clip-text bg-gradient-to-r from-yellow-200 via-red-300 via-amber-300 to-purple-500 hover:brightness-90`}>Destinasyon.app</Link>
+              <Link href="/" className={`text-2xl transition-all duration-300 font-bold select-none cursor-pointer ${erode.className} inline-block text-transparent  bg-clip-text bg-gradient-to-r from-yellow-200 via-red-300 via-amber-300 to-purple-500 hover:brightness-90`}>
+                Destinasyon.app
+              </Link>
             </div>
 
             <div className="w-4/6 sm:w-fit md:w-1/3 order-3 flex justify-end">
@@ -34,7 +32,9 @@ export default function RootLayout({
           {children}
 
           <footer className="w-full text-center p-4 my-2">
-            <a href="https://ricafolio.me" target="_blank" rel="noreferrer" className="text-gray-500 hover:text-gray-300 transition-colors">made with 💛 by ricafolio</a>
+            <a href="https://ricafolio.me" target="_blank" rel="noreferrer" className="text-gray-500 hover:text-gray-300 transition-colors">
+              made with 💛 by ricafolio
+            </a>
           </footer>
         </div>
       </body>
