a5aa937ccfa831166362636bceed96647f7d6b1c
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-04-16 13:50:58 +0800

    refactor: move args to an exported interface

--- src/app/types/props.ts
+++ src/app/types/props.ts
@@ -1,21 +1,34 @@
 import { Destination, SpotSaved } from "./index"
 
+// Args
+
+export interface PromptValueChangeArgs {
+  newValue: string
+  isClear: boolean
+}
+
+export interface DeleteSpotArgs {
+  id: number,
+  name: string
+}
+
+export interface SaveSpotArgs {
+  destination: string
+  name: string
+  description: string
+  imageUrl: string
+}
+
+// Props
+
 export interface DestinationProps extends Destination {
   index: number
-  onSaveBtnClick: (
-    destinationName: string, 
-    name: string, 
-    description: string, 
-    imageUrl: string
-  ) => void
+  onSaveBtnClick: (...args: SaveSpotArgs[]) => void
 }
 
 export interface SpotSavedProps extends SpotSaved {
   id: number
-  onDeleteBtnClick: (
-    id: number,
-    name: string
-  ) => void
+  onDeleteBtnClick: (...args: DeleteSpotArgs[]) => void
 }
 
 export interface InputProps {
@@ -23,8 +36,5 @@ export interface InputProps {
   isFetching: boolean
   onRandomBtnClick: () => void
   onSubmitBtnClick: () => void
-  onPromptValueChange: (
-    newValue: string,
-    isClear: boolean
-  ) => void
+  onPromptValueChange: (...args: PromptValueChangeArgs[]) => void
 }
