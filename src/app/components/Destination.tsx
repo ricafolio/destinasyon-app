37d9ca3d6495bbd07ff7dbc3f9233c550db703b1
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-04-17 15:24:42 +0800

    chore: create new component for Spot

--- src/app/components/Destination.tsx
+++ src/app/components/Destination.tsx
@@ -1,17 +1,11 @@
-import Image from "next/image"
 import { useState, useEffect } from "react"
 import { Fade } from "react-awesome-reveal";
 import { Spot } from "../types"
 import { DestinationProps } from "../types/props"
-
-import SaveIcon from "./icons/SaveIcon"
-import ExternalIcon from "./icons/ExternalIcon"
-import CheckIcon from "./icons/CheckIcon"
+import DestinationSpot from "./DestinationSpot";
 
 export default function Destination({ name, description, spots, index, onSaveBtnClick }: DestinationProps) {
   const [updatedSpots, setUpdatedSpots] = useState<Spot[]>(spots)
-  const [saved, setSaved] = useState<boolean>(false)
-  const [permanentlyHidden, setPermanentlyHidden] = useState<boolean>(false)
 
   useEffect(() => {
     // run once on mount
@@ -36,11 +30,6 @@ export default function Destination({ name, description, spots, index, onSaveBtn
       }
     }
 
-  function permanentlyHideAfterSave() {
-    setTimeout(() => {
-      setPermanentlyHidden(true)
-    }, 4000)
-  }
 
   return (
     <div className="text-left mb-24">
@@ -53,61 +42,18 @@ export default function Destination({ name, description, spots, index, onSaveBtn
         <Fade triggerOnce={true} cascade={true} damping={0.5} className="group w-full sm:w-[calc(50%_-_8px)] lg:w-[calc(33.33%_-_8px)] flex flex-col bg-white rounded-lg relative">
           {updatedSpots.map((spot: Spot, i: number) => {
             return (
-              <div key={`spot-${index}-${i}`}>
-                <div className="w-full h-48 relative mb-2 bg-gray-100 rounded-md">
-                  <Image 
-                    src={spot.imageUrl || "./empty.svg"} 
-                    alt={spot.name} 
-                    fill={true} 
-                    style={{ objectFit: "cover" }} 
-                    className="w-full rounded-t-md" 
-                  />
-                </div>
-
-                <section className="px-3 pt-1 pb-2 sm:pb-4">
-                  <h2 className="text-xl font-semibold text-black">
-                    <a href={spot.mapsUrl} target="_blank" rel="noreferrer" className="transition-colors duration-200 hover:text-gray-600 inline-block cursor-pointer">
-                      <span>
-                        {spot.name} 
-                        <span className="inline-block align-middle text-gray-400 hover:scale-100 ml-1">
-                          <ExternalIcon />
-                        </span>
-                      </span>
-                    </a>
-                  </h2>
-                  <p className="text-sm text-gray-700 mt-1 mb-2 md:mb-0">
-                    {spot.description}
-                  </p>
-                </section>
-
-                <div className={`${!permanentlyHidden ? 'group-hover:block' : 'group-hover:hidden'} hidden absolute top-0 right-2 my-2`}>
-                  <button
-                    className={`${!saved ? 'bg-red-700 hover:bg-red-800' : 'bg-green-600'} text-white px-4 py-2 rounded transition-colors duration-200 inline-flex items-center justify-center`}
-                    onClick={() => {
-                      onSaveBtnClick({
-                        name: spot.name,
-                        description: spot.description,
-                        imageUrl: spot.imageUrl || "./empty.svg",
-                        mapsUrl: spot.mapsUrl,
-                        uid: spot.uid,
-                        vicinity: name,
-                        rating: spot.rating,
-                        totalRatings: spot.totalRatings,
-                      })
-                      setSaved(true)
-                      permanentlyHideAfterSave()
-                    }}
-                    disabled={saved}
-                  >
-                    <span className="mr-1">
-                      {!saved ? <SaveIcon /> : <CheckIcon />}
-                    </span>
-                    <span>
-                      {!saved ? "Save" : "Saved!"}
-                    </span>
-                  </button>
-                </div>
-              </div>
+              <DestinationSpot
+                key={`spot-${index}-${i}`}
+                name={spot.name}
+                description={spot.description}
+                imageUrl={spot.imageUrl}
+                mapsUrl={spot.mapsUrl}
+                uid={spot.uid}
+                vicinity={spot.vicinity}
+                rating={spot.rating}
+                totalRatings={spot.totalRatings}
+                onSaveBtnClick={onSaveBtnClick}
+              />
             )
           })}
         </Fade>
---
+++ src/app/components/DestinationSpot.tsx
@@ -0,0 +1,79 @@
+import { useState } from "react"
+import { Spot } from "../types"
+import Image from "next/image"
+
+import SaveIcon from "./icons/SaveIcon"
+import ExternalIcon from "./icons/ExternalIcon"
+import CheckIcon from "./icons/CheckIcon"
+
+interface SpotExtended extends Spot {
+  onSaveBtnClick: (...args: Spot[]) => void
+}
+
+export default function DestinationSpot({
+  name,
+  description,
+  imageUrl,
+  mapsUrl,
+  uid,
+  vicinity,
+  rating,
+  totalRatings,
+  onSaveBtnClick
+}: SpotExtended) {
+  const [saved, setSaved] = useState<boolean>(false)
+  const [permanentlyHidden, setPermanentlyHidden] = useState<boolean>(false)
+
+  function permanentlyHideAfterSave() {
+    setTimeout(() => {
+      setPermanentlyHidden(true)
+    }, 4000)
+  }
+
+  return (
+    <div>
+      <div className="w-full h-48 relative mb-2 bg-gray-100 rounded-md">
+        <Image src={imageUrl || "./empty.svg"} alt={name} fill={true} style={{ objectFit: "cover" }} className="w-full rounded-t-md" />
+      </div>
+
+      <section className="px-3 pt-1 pb-2 sm:pb-4">
+        <h2 className="text-xl font-semibold text-black">
+          <a href={mapsUrl} target="_blank" rel="noreferrer" className="transition-colors duration-200 hover:text-gray-600 inline-block cursor-pointer">
+            <span>
+              {name}
+              <span className="inline-block align-middle text-gray-400 hover:scale-100 ml-1">
+                <ExternalIcon />
+              </span>
+            </span>
+          </a>
+        </h2>
+
+        <p className="text-sm text-gray-700 mt-1 mb-2 md:mb-0">{description}</p>
+      </section>
+
+      <div className={`${!permanentlyHidden ? "group-hover:block" : "group-hover:hidden"} hidden absolute top-0 right-2 my-2`}>
+        <button
+          className={`${!saved ? "bg-red-700 hover:bg-red-800" : "bg-green-600"} text-white px-4 py-2 rounded transition-colors duration-200 inline-flex items-center justify-center`}
+          onClick={() => {
+            onSaveBtnClick({
+              name: name,
+              description: description,
+              imageUrl: imageUrl || "./empty.svg",
+              mapsUrl: mapsUrl,
+              uid: uid,
+              vicinity: vicinity,
+              rating: rating,
+              totalRatings: totalRatings
+            })
+            setSaved(true)
+            permanentlyHideAfterSave()
+          }}
+          disabled={saved}
+        >
+          <span className="mr-1">{!saved ? <SaveIcon /> : <CheckIcon />}</span>
+          <span>{!saved ? "Save" : "Saved!"}</span>
+        </button>
+      </div>
+    </div>
+  )
+}
