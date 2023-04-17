2406a464ed5c795323277173ab58d42e370f2018
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-04-17 16:40:00 +0800

    refactor: move loading icon to component

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,4 +1,5 @@
 import { InputProps } from "../types/props"
+import LoadingIcon from "./icons/LoadingIcon"
 import RandomIcon from "./icons/RandomIcon"
 
 export default function Input({ prompt, fetchStatus, onRandomBtnClick, onSubmitBtnClick, onPromptValueChange }: InputProps) {
@@ -34,12 +35,7 @@ export default function Input({ prompt, fetchStatus, onRandomBtnClick, onSubmitB
         </div>
 
         <button className="w-full sm:w-60 flex items-center justify-center rounded-lg px-8 py-2 mt-2 sm:ml-2 sm:mt-0 text-lg sm:text-xl h-auto transition-colors duration-150 focus:outline-6  disabled:cursor-wait focus:outline-amber-500 bg-amber-400 text-black hover:bg-amber-500 active:bg-amber-400 focus:bg-amber-500" onClick={onSubmitBtnClick} disabled={fetchStatus.isLoading}>
-          {fetchStatus.isLoading && fetchStatus.source === 'submit' ? (
-            <svg className="animate-spin mx-auto h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-              <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-            </svg>
-          ) : 'Get destinations'}
+          {fetchStatus.isLoading && fetchStatus.source === 'submit' ? <LoadingIcon /> : 'Get destinations'}
         </button>
       </div>
     </div>
