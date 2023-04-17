20f738571ff69aba4e1a9f07f266c6a78ef2412e
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-04-17 17:14:47 +0800

    fix: displayed null ratings

--- src/app/components/icons/StarIcon.tsx
+++ src/app/components/icons/StarIcon.tsx
@@ -1,6 +1,7 @@
 export default function StarIcon({ rating, totalRatings } : { rating: number, totalRatings: number }) {
   const maxRating = 5.0 // or 10.0
-  const ratingPercentage = (rating / maxRating) * 100
+  const actualRating = rating ? rating : 0
+  const ratingPercentage = (actualRating / maxRating) * 100
 
   return (
     <div className="flex items-center mb-1">
@@ -9,7 +10,7 @@ export default function StarIcon({ rating, totalRatings } : { rating: number, to
         <div className="filled-stars" style={{ width: `${ratingPercentage}%` }}></div>
       </div>
       <div className="text-gray-500 text-sm ml-1">
-        ({totalRatings})
+        ({totalRatings ? totalRatings : 0})
       </div>
     </div>
   )
