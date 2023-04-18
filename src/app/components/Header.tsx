58ac224be4c050a6c908b88d95938a7c26f62bf5
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-04-18 19:31:38 +0800

    chore: move menu icon to a component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,6 +4,7 @@ import { erode } from "../fonts"
 
 import Link from "next/link"
 import NavLink from "./NavLink"
+import MenuOpen from "./icons/MenuOpen"
 
 export default function Header() {
   const [open, setOpen] = useState<boolean>(false)
@@ -21,7 +22,7 @@ export default function Header() {
 
         <div className="w-1/2 sm:hidden flex justify-end">
           <button onClick={() => setOpen(!open)} className="transition-colors duration-200 bg-zinc-800 hover:bg-zinc-800/80 rounded px-1">
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 15 15"><path fill="#eee" fillRule="evenodd" d="M2.5 4a.5.5 0 0 0 0 1h10a.5.5 0 0 0 0-1h-10ZM2 7.5a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1h-10a.5.5 0 0 1-.5-.5Zm0 3a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1h-10a.5.5 0 0 1-.5-.5Z" clipRule="evenodd"/></svg>
+            <MenuOpen />
           </button>
         </div>
       </div>
